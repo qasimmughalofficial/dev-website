7e643db547250729719ff68e817f439e5eb4d726
qasimmughalofficial <qasimmughalofficial@gmail.com>
2026-02-25 15:45:07 +0500

    Footer Done

--- components/Footer/page.tsx
+++ components/Footer/page.tsx
@@ -1,66 +1,39 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./footer.module.css";
 
 const currentYear = new Date().getFullYear();
 
 const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.footerTop}>
-        <h3 className={styles.footerLogo}>DevCritters</h3>
-        <div className={styles.footerSocial}>
-          {/* External links use plain <a> */}
-          <a
-            href="https://www.facebook.com/devcritters"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/facebook.svg"
-              alt="facebook"
-              width={20}
-              height={20}
-            />
-          </a>
-          <a href="https://x.com/devcritters" target="_blank" rel="noopener noreferrer">
-            <Image
-              src="/images/twitter.svg"
-              alt="twitter"
-              width={20}
-              height={20}
-            />
-          </a>
-          <a
-            href="https://www.instagram.com/devcritters/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/instagram.svg"
-              alt="instagram"
-              width={20}
-              height={20}
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.footerBottom}>
-        <p className={styles.footerRights}>
-          &copy; {currentYear} - All Rights Reserved by DevCritters
-        </p>
-        <div className={styles.footerLinks}>
-          {/* Internal links use Next.js Link */}
-          <a href="/">
-            <span>Privacy policy</span>
-          </a>
-          <a href="/">
-            <span>Terms & conditions</span>
-          </a>
-        </div>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className={styles.footerContainer}>
+            <div className="theme-container">
+                <div className={styles.footerInner}>
+                    <div className={styles.topSection}>
+                        <h3 className={styles.logo}>DevCritters</h3>
+                        <div className={styles.socialIcons}>
+                            <Link href="https://www.facebook.com/devcritters">
+                                <Image className={styles.footerIcon} src="/images/facebook.svg" alt="facebook" width={15} height={20} />
+                            </Link>
+                            <Link href="https://x.com/devcritters">
+                                <Image className={styles.footerIcon} src="/images/twitter.svg" alt="twitter" width={20} height={20} />
+                            </Link>
+                            <Link href="https://www.instagram.com/devcritters/">
+                                <Image className={styles.footerIcon} src="/images/instagram.svg" alt="instagram" width={20} height={20} />
+                            </Link>
+                        </div>
+                    </div>
+                    <div className={styles.bottomSection}>
+                        <p className={styles.bottomText}>&copy; {currentYear} - All Rights Reserved by DevCritters</p>
+                        <div className={styles.bottomLinks}>
+                            <Link href="/">Privacy policy</Link>
+                            <Link href="/">Terms & conditions</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
